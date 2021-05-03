a17733bd811af1bc88a5cd15ff0af62a565e23fa
Divyesh Parmar <divyeshpar2496@gmail.com>
2021-05-03 23:32:25 +0530

    chart addition

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import './App.css';
 import { useEffect, useRef, useState } from 'react';
 import DataTable from './DataTable';
-
-const updatedList = (old, newList) => {
-  const remaining = [...newList];
-  const delta = old.map((city) => {
-    const appleIndex = remaining.findIndex((apple) => apple.city === city.city);
-    if (appleIndex >= 0) {
-      const [apple] = remaining.splice(appleIndex, 1);
-      return {
-        ...city,
-        aqi: apple.aqi,
-        timestamp: parseInt(new Date().getTime() / 1000),
-      };
-    }
-    return { ...city, timestamp: parseInt(new Date().getTime() / 1000) };
-  });
-  return delta.concat(remaining);
-};
+import ChartWrapper from './ChartWrapper';
 
 function App() {
   const ws = useRef(null);
   const [fetchedData, setFD] = useState([]);
+  const [individualAqi, setIndividual] = useState([]);
+  const [individualCity, setCityName] = useState('');
+
+  const updatedList = (old, newList) => {
+    const remaining = [...newList];
+    const delta = old.map((city, ctIndex) => {
+      const appleIndex = remaining.findIndex((apple) => apple.city === city.city);
+      if (appleIndex >= 0) {
+        const [apple] = remaining.splice(appleIndex, 1);
+        if (ctIndex === appleIndex) {
+          setIndividual((p) => [...p, apple.aqi]);
+          setCityName(city.city);
+        }
+        return {
+          ...city,
+          aqi: apple.aqi,
+          timestamp: parseInt(new Date().getTime() / 1000),
+        };
+      }
+      return { ...city, timestamp: parseInt(new Date().getTime() / 1000) };
+    });
+    return delta.concat(remaining);
+  };
 
   const handleMessage = (e) => {
     const apples = JSON.parse(e.data);
@@ -41,6 +48,7 @@ function App() {
     <div className='App'>
       <h2>Air Quality Index app</h2>
       <DataTable data={fetchedData} />
+      <ChartWrapper individualCity={individualCity} aqiData={individualAqi} />
     </div>
   );
 }
---
+++ src/ChartWrapper.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Chartjs from 'chart.js';
+
+const chartConfig = {
+  type: 'line',
+  data: {
+    labels: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    datasets: [
+      {
+        label: 'AQI indices of city',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
+};
+
+const ChartWrapper = ({ individualCity, aqiData }) => {
+  const chartContainer = useRef(null);
+  const [chartInstance, setChartInstance] = useState(null);
+
+  const updateDataset = (datasetIndex, newData) => {
+    if (!chartInstance || !chartInstance.data) return;
+    chartInstance.data.labels = [
+      ...chartInstance.data.labels,
+      chartInstance.data.labels.length + 1,
+    ];
+    chartInstance.data.datasets[datasetIndex].label = `AQI indices of ${individualCity}`;
+    chartInstance.data.datasets[datasetIndex].data = newData;
+    chartInstance.update();
+  };
+
+  useEffect(() => {
+    if (chartContainer && chartContainer.current) {
+      const newChartInstance = new Chartjs(chartContainer.current, {
+        ...chartConfig,
+        dataSets: { ...chartConfig, data: aqiData },
+      });
+      setChartInstance(newChartInstance);
+    }
+  }, [chartContainer]);
+
+  useEffect(() => {
+    if (!aqiData) return;
+    updateDataset(0, aqiData);
+  }, [aqiData]);
+
+  return (
+    <div className='chart-wrapper'>
+      <canvas ref={chartContainer} />
+    </div>
+  );
+};
+
+export default ChartWrapper;
